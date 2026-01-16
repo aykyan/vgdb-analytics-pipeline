3688be685a1d9c9d135f775c4f0957ec9dd379b0
Galactus <galactus@enimasoft.com>
2026-01-17 01:21:47 +0400

    Fix bugs in model scripts; add correct visualizations.

--- src/models/m_genre_summary.py
+++ src/models/m_genre_summary.py
@@ -8,13 +8,15 @@ STAGING_DIR   = Path("data/processed/staging")
 ANALYTICS_DIR = Path("data/processed/analytics")
 
 STG_GAME_INFO_PATH   = STAGING_DIR / "stg_game_info.parquet"
-M_GENRE_SUMMARY_PATH = ANALYTICS_DIR / "models/m_games_by_platform.parquet"
+M_GENRE_SUMMARY_PATH = ANALYTICS_DIR / "models/m_genre_summary.parquet"
+
+stg = pl.read_parquet(STG_GAME_INFO_PATH)
 
 model = (
 	stg.explode("genres")
-       .groupby("genres")
+       .group_by("genres")
        .agg([
-           pl.count("id").alias("total_games"),
+           pl.count("game_id").alias("total_games"),
            pl.mean("rating").alias("avg_rating")
        ])
 )
